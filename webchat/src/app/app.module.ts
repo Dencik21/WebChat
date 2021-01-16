3fe249af7548a174f2be14118ded617ff1f568dd
Rodion Kovalenko <rodion_kovalenko@yahoo.de>
2021-01-16 18:35:24 +0100

    menu bar

--- webchat/src/app/app.module.ts
+++ webchat/src/app/app.module.ts
@@ -1,9 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { environment } from '../environments/environment';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule, LocationStrategy, HashLocationStrategy } from "@angular/common";
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { AppRoutingModule } from './app-routing.module';
+import { enableProdMode } from '@angular/core';
+import { Router, RouteReuseStrategy } from '@angular/router';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { VideoComponent } from './video/video.component';
+
+enableProdMode();
 
 @NgModule({
   declarations: [
@@ -12,9 +26,32 @@ import { VideoComponent } from './video/video.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSnackBarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [  
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(router: Router) {
+    router.navigateByUrl('/', { skipLocationChange: false }).then(() => {
+      router.navigate(['/']);
+    });
+  }
+}
--- webchat/src/app/video/video.component.ts
+++ webchat/src/app/video/video.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit, ViewEncapsulation, ViewChild, ElementRef, OnDestroy, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSnackBarVerticalPosition } from "@angular/material/snack-bar";
+
 
 @Component({
   selector: 'app-video',
@@ -7,16 +10,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./video.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class VideoComponent implements OnInit {
-  @ViewChild('video') video: ElementRef | undefined;
-  loading: boolean | undefined;
+export class VideoComponent implements OnInit, AfterViewInit  {
+  @ViewChild('video') video: any;
+  loading: any;
   predictions: any;
   frontCameraActive: any;
   spinnerValue = 100;
 
-  constructor(private router: Router) {
-    router.events.subscribe((event) => {
-    });
+ 
+  constructor(private _snackBar: MatSnackBar, private router: Router) {     
+    // router.events.subscribe((event) =>{
+      
+    // });
   }
 
   async ngOnInit() {
@@ -25,10 +30,16 @@ export class VideoComponent implements OnInit {
     this.frontCameraActive = true;
     this.onTurnCamera(null);
   }
+  
+  async ngAfterViewInit() {    
+  }  
 
   startCamera(constraints: any) {
-    const vid = this.video.nativeElement;
+  let vid: any;
 
+  if (this.video && this.video.nativeElement) {
+    vid = this.video.nativeElement;
+  }
     if (!constraints) {
       constraints = true;
     }
@@ -41,6 +52,11 @@ export class VideoComponent implements OnInit {
         })
         .catch((err0r) => {
           console.log('Something went wrong!');
+          this.showToastMessage(
+          'camera not',
+           'it was not possible to detect a camera on your device.',
+           1000      
+           );
         });
     }
   }
@@ -57,4 +73,33 @@ export class VideoComponent implements OnInit {
     this.frontCameraActive = !this.frontCameraActive;
     this.startCamera(constraints);
   }
+  
+   showToastMessage(title: any, message: any, duration: any) {
+    // this._snackBar.open(title, message, 
+    //   { duration: duration, direction: 'ltr', verticalPosition: 'top' });
+
+      this._snackBar.openFromComponent(SnackbarComponent, {
+        duration: 1000,
+        direction: 'ltr',
+        verticalPosition: 'top' as MatSnackBarVerticalPosition
+      });
+  }
+}
+
+@Component({
+  selector: 'snack-bar-component',
+  template: '<span class="example-pizza-party">' +
+              '{{message}}'+
+            '</span>',
+  styles: [`
+    .example-pizza-party {
+      color: hotpink;
+    }
+  `],
+})
+
+export class SnackbarComponent {
+  title: any;
+  message: any = 'Cannot open camera on the device';
+  duration: any = 1000;
 }
